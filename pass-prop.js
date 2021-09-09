d8dc8485aad222e836c3c82681ec26787b47c6a1
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-09-09 19:48:43 -0400

    ...

--- pass-prop.js
+++ pass-prop.js
@@ -3,7 +3,10 @@ import { def } from 'trans-render/lib/def.js';
 export class PassProp extends PassDown {
     static is = 'pass-prop';
     attach(elementToObserve, { on, handleEvent }) {
-        const prop = Object.getOwnPropertyDescriptor(elementToObserve.constructor.prototype, on);
+        let prop = Object.getOwnPropertyDescriptor(elementToObserve, on);
+        if (prop === undefined) {
+            prop = Object.getOwnPropertyDescriptor(elementToObserve.constructor.prototype, on);
+        }
         const setter = prop.set.bind(elementToObserve);
         const getter = prop.get.bind(elementToObserve);
         Object.defineProperty(elementToObserve, on, {
